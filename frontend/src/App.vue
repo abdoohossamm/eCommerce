1d7f00bbff61f10a2d7d15a0e4e38bfb724c4fde
abdoohossamm <abdoohossamm@outlook.com>
2022-11-08 14:05:25 +0200

    finish the Vue base template (nav and footer)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,48 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="wrapper">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item"><strong>eCommerce</strong></router-link>
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+        <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu}">
+          <div class="navbar-end">
+            <router-link to="/" class="navbar-item">summer</router-link>
+            <router-link to="/" class="navbar-item">winter</router-link>
+
+            <div class="navbar-item">
+              <div class="buttons">
+                <router-link to="/login" class="button is-light">Login</router-link>
+                <router-link to="/cart" class="button is-success">
+                  <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+                  <span>Cart</span>
+                </router-link>
+              </div>
+            </div>
+          </div>
+        </div>
+    </nav>
+    <section class="section">
+    <router-view/>
+    </section>
+    <footer class="footer">
+      <p class="has-text-centered">Copyright Abdoohossamm (c) 2022</p>
+    </footer>
+  </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  data(){
+    return{
+      showMobileMenu: false,
     }
   }
 }
+</script>
+<style lang="scss">
+@import "../node_modules/bulma";
 </style>
