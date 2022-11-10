d7df7921b08bc86d616046b29694c4f3c4e4f023
abdoohossamm <abdoohossamm@outlook.com>
2022-11-10 06:52:22 +0200

    Setup django restframework and basic serializers.

--- main_backend/src/settings.py
+++ main_backend/src/settings.py
@@ -167,6 +167,23 @@ class Dev(Configuration):
     CORS_ALLOWED_ORIGINS = [
         'http://localhost:8080'
     ]
+    REST_FRAMEWORK = {
+        "DEFAULT_AUTHENTICATION_CLASSES": [
+            "rest_framework.authentication.BasicAuthentication",
+            "rest_framework.authentication.SessionAuthentication",
+            "rest_framework.authentication.TokenAuthentication",
+            "rest_framework_simplejwt.authentication.JWTAuthentication"
+        ],
+        "DEFAULT_PERMISSION_CLASSES": [
+            "rest_framework.permissions.IsAuthenticatedOrReadOnly"
+        ],
+        "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+        "PAGE_SIZE": 20,
+        # "DEFAULT_FILTER_BACKENDS": [
+        #     "django_filters.rest_framework.DjangoFilterBackend",
+        #     'rest_framework.filters.OrderingFilter'
+        # ],
+    }
 
 
 class Prod(Dev):
---
+++ main_backend/store/api/v1/serializers.py
@@ -0,0 +1,102 @@
+from rest_framework import serializers
+from store.models import Category, Album, Review, Product
+from django.contrib.auth import get_user_model
+from drf_extra_fields.fields import Base64ImageField
+
+User = get_user_model()
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
+        readonly = ["modified_at", "created_at"]
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name", "username", "email"]
+
+
+class ImagesSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+    image = Base64ImageField(required=False)
+
+    class Meta:
+        model = Album
+        fields = ["id", "image", "created_by"]
+
+
+class AlbumSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+    image = Base64ImageField(required=False)
+
+    class Meta:
+        model = Album
+        fields = "__all__"
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+    created_by = serializers.HyperlinkedRelatedField(
+        queryset=User.objects.all(),
+        view_name="api_user_detail",
+        lookup_field="username",
+
+    )
+
+    class Meta:
+        model = Review
+        fields = ["id", "created_by", "content", 'rate', "modified_at", "created_at"]
+        readonly = ["modified_at", "created_at"]
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    created_by = serializers.HyperlinkedRelatedField(
+        queryset=User.objects.all(),
+        view_name="api_user_detail",
+        lookup_field="username"
+    )
+    thumbnail = Product.get_thumbnail
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+        readonly = ["modified_at", "created_at"]
+
+
+class ProductDetailSerializer(ProductSerializer):
+    reviews = ReviewSerializer(many=True)
+    images = ImagesSerializer(many=True)
+
+    def update(self, instance, validated_data):
+        images = validated_data.pop('images')
+        reviews = validated_data.pop("reviews")
+        instance = super(ProductDetailSerializer, self).update(instance, validated_data)
+
+        for review_data in reviews:
+            if review_data.get("id"):
+                # review has an ID so was pre-existing
+                continue
+            review = Review(**review_data)
+            review.created_by = self.context["request"].user
+            review.content_object = instance
+            review.save()
+
+        for image_data in images:
+            if image_data.get("id"):
+                # image has an ID so was pre-existing
+                continue
+
+            image64 = image_data.get('image')
+            image = Album(**image_data)
+            image.content_object = instance
+            image.created_by = image.content_object.created_by
+            image.image = image64
+            image.save()
+            try:
+                image.content_object.generate_thumbnail()
+            except Exception as e:
+                raise e
+        return instance
