29d61a95071ac21f12939c90bf6afefa9bc09527
abdoohossamm <abdoohossamm@outlook.com>
2022-11-10 05:36:30 +0200

    add nginx to serve static and media files for backend

--- main_backend/entrypoint.sh
+++ main_backend/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 python manage.py migrate --no-input
+python manage.py collectstatic --no-input
 celery -A src worker -l INFO --detach
-gunicorn src.wsgi --reload --bind 0.0.0.0:8000
+gunicorn src.wsgi --reload --bind 0.0.0.0:8001
