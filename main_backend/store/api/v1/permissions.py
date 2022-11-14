73fab6b63a9d270609b19e30877562a57d9fa734
abdoohossamm <abdoohossamm@outlook.com>
2022-11-14 12:04:09 +0200

    Disallow any authenticated users from creating items

--- main_backend/store/api/v1/permissions.py
+++ main_backend/store/api/v1/permissions.py
@@ -2,6 +2,21 @@ from rest_framework import permissions
 
 
 class CreatorModifyOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
+    """
+    Allow Object creator to modify, or delete the object but not create
+    """
+    def has_permission(self, request, view):
+        """
+        This allows see, modify, delete but not create except for superuser
+        """
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        if request.user and request.user.is_authenticated:
+            return bool((request.user and request.user.is_staff) or
+                        (request.user.is_seller and request.method in ['PUT', 'PATCH', 'DELETE'])
+                        )
+        return False
+
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
