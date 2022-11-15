daca872c45e580c44c9eaf273727dacce664db77
abdoohossamm <abdoohossamm@outlook.com>
2022-11-15 14:26:13 +0200

    create Order model to track the orders and payments and OrderItem model to truck the products inside the model

---
+++ main_backend/orders/models.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+
+from django.db import models
+from django.contrib.auth import get_user_model
+
+from store.models import Product
+
+User = get_user_model()
+
+
+class Order(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    address = models.CharField(max_length=200)
+    zipcode = models.PositiveIntegerField()
+    place = models.CharField(max_length=200)
+    phone = models.CharField(max_length=100)
+
+    paid_amount = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
+    payment_token = models.CharField(max_length=200)
+    created_by = models.ForeignKey(User, related_name='order_creator', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at',]
+
+    def __str__(self):
+        return f"{self.first_name} at:{self.created_at}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='items', on_delete=models.PROTECT)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.id}"
