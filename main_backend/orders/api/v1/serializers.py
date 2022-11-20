001c6d42019a47da15e07ce35da6ea3ac32f416b
abdoohossamm <abdoohossamm@outlook.com>
2022-11-20 19:05:06 +0200

    Create OrderSerializer for model order

--- main_backend/orders/api/v1/serializers.py
+++ main_backend/orders/api/v1/serializers.py
@@ -12,3 +12,36 @@ class OrderItemSerializer(serializers.ModelSerializer):
             "quantity",
             "price",
         )
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True)
+
+    class Meta:
+        model = Order
+        fields = (
+            "first_name",
+            "last_name",
+            "email",
+            "address",
+            "zipcode",
+            "place",
+            "phone",
+            "paid_amount",
+            "payment_token",
+            "paid",
+            "delivered",
+            "cash_on_delivery",
+            "items",
+            "created_by",
+            "created_at",
+            "modified_at",
+        )
+        read_only_fields = ("payment_token", "created_by", "created_at", "modified_at",)
+
+    def create(self, validated_data):
+        items_data = validated_data.pop('items')
+        order = Order.objects.create(**validated_data)
+        for items_data in items_data:
+            OrderItem.objects.create(order=order, **items_data)
+        return order
