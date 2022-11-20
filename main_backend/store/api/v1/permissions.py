f266a0288b4e93bd8b8b3560f80b90fffb7e5c87
abdoohossamm <abdoohossamm@outlook.com>
2022-11-20 19:10:26 +0200

    Add new permissions for the Store application API

--- main_backend/store/api/v1/permissions.py
+++ main_backend/store/api/v1/permissions.py
@@ -2,9 +2,23 @@ from rest_framework import permissions
 
 
 class CreatorModifyOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return request.user == obj.created_by
+
+
+class IsAdminUserForObject(permissions.IsAdminUser):
+    def has_object_permission(self, request, view, obj):
+        return bool(request.user and request.user.is_staff)
+
+
+class SellerModifyOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
     """
     Allow Object creator to modify, or delete the object but not create
     """
+
     def has_permission(self, request, view):
         """
         This allows see, modify, delete but not create except for superuser
@@ -24,6 +38,56 @@ class CreatorModifyOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
         return request.user == obj.created_by
 
 
-class IsAdminUserForObject(permissions.IsAdminUser):
+class IsSellerUser(permissions.IsAuthenticatedOrReadOnly):
+    def has_permission(self, request, view):
+        if request.method in ["POST", 'PUT', 'PATCH', 'DELETE'] and request.user.is_authenticated:
+            return bool(request.user and request.user.is_seller)
+        return bool(
+            request.method in permissions.SAFE_METHODS or
+            request.user and
+            request.user.is_superuser
+
+        )
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return request.user == obj.created_by
+
+
+class UserDetailReadOnly(permissions.IsAuthenticatedOrReadOnly):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        if request.user and request.user.is_authenticated:
+            return bool((request.user and request.user.is_staff) or
+                        (request.user.is_seller and request.method in ['PUT', 'PATCH', 'DELETE'])
+                        )
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return request.user == obj
+
+
+class IsReviewReadOnly(permissions.IsAuthenticatedOrReadOnly):
+    def has_permission(self, request, view):
+        reviews = request.data.get('reviews', 0)
+        if request.method in ["PATCH"] and len(request.data) == 1 and \
+                reviews and "id" not in reviews[0] and request.user.is_authenticated:
+            return True
+        return False
+
+
+class CategoryPermission(permissions.IsAdminUser):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        else:
+            return bool(request.user and request.user.is_staff)
+
     def has_object_permission(self, request, view, obj):
         return bool(request.user and request.user.is_staff)
