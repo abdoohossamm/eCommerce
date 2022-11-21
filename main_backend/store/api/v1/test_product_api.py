24788abea635e653529885ada9ba7f0acc70dbd1
abdoohossamm <abdoohossamm@outlook.com>
2022-11-21 19:34:14 +0200

    Create test cases for product API

---
+++ main_backend/store/api/v1/test_product_api.py
@@ -0,0 +1,200 @@
+from datetime import datetime
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.utils import timezone
+from pytz import UTC
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient
+
+from store.models import Product, Category
+
+
+class ProductApiTestCase(TestCase):
+    """
+    A test case class to test Products API.
+    Test cases:
+        test_product_list()
+        test_unauthenticated_product_create()
+        test_customer_product_create()
+        test_seller_product_create()
+        test_post_create()
+    TODO: Check update products with PATCH and PUT request with superuser
+    TODO: Check update products with PATCH and PUT request with the product's seller
+    """
+    def setUp(self):
+        """
+        Initial setup with 3 users (admin, seller, customer), 2 Categories, 2 Products, and API Client.
+        """
+        self.seller = get_user_model().objects.create_user(
+            email="test@example.com", username='seller', password="password", is_seller=True
+        )
+        self.admin = get_user_model().objects.create_user(
+            email="testadmin@example.com", username='admin', password="password3", is_superuser=True
+        )
+
+        self.user = get_user_model().objects.create_user(
+            email="test2@example.com", username='user', password="password2"
+        )
+        categories = [
+            Category.objects.create(
+                name='Clothes',
+                slug='clothes',
+                created_at=timezone.now(),
+                created_by=self.admin
+            ),
+            Category.objects.create(
+                name='Laptops',
+                slug='laptops',
+                created_at=timezone.now(),
+                created_by=self.admin
+            ),
+        ]
+        products = [
+            Product.objects.create(
+                category=categories[0],
+                title="T-shirt",
+                slug='t-shirt',
+                description='Green T-shirt',
+                price=125.25,
+                created_by=self.seller,
+                in_stock=True,
+                is_active=True,
+                created_at=timezone.now()
+            ),
+            Product.objects.create(
+                category=categories[1],
+                title="Asus TUF F-15",
+                slug='asus_tuf_f-15',
+                description='8GB RAM 15.6" FHD display',
+                price=12000.00,
+                created_by=self.admin,
+                in_stock=True,
+                is_active=True,
+                created_at=timezone.now()
+            ),
+        ]
+
+        # let us look up the post info by slug
+        self.product_lookup = {p.slug: p for p in products}
+
+        # override test client
+        self.client = APIClient()
+        self.admin_token = Token.objects.create(user=self.admin)
+        self.user_token = Token.objects.create(user=self.user)
+        self.seller_token = Token.objects.create(user=self.seller)
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.user_token.key)
+
+    def test_product_list(self):
+        """
+        Test the product api link with GET request to check if it returns a list or products as a json response.
+        Check if the products return are the products in the setUp().
+        """
+        resp = self.client.get("/api/v1/products/")
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.headers['Content-Type'], 'application/json')
+        data = resp.json()["results"]
+        self.assertEqual(len(data), 2)
+
+        for product_dict in data:
+            product_obj = self.product_lookup[product_dict["slug"]]
+            self.assertEqual(product_obj.title, product_dict["title"])
+            self.assertEqual(product_obj.slug, product_dict["slug"])
+            self.assertEqual(product_obj.description, product_dict["description"])
+            self.assertEqual(product_obj.price, float(product_dict["price"]))
+            self.assertEqual(product_obj.in_stock, product_dict["in_stock"])
+            self.assertEqual(product_obj.is_active, product_dict["is_active"])
+            self.assertTrue(
+                product_dict["created_by"].endswith(f"/api/v1/users/{product_obj.created_by.username}")
+            )
+            self.assertEqual(
+                product_obj.created_at,
+                datetime.strptime(
+                    product_dict["created_at"], "%Y-%m-%dT%H:%M:%S.%fZ"
+                ).replace(tzinfo=UTC),
+            )
+
+    def test_unauthenticated_product_create(self):
+        """
+        Check if the unauthenticated user can create a product
+        """
+        # unset credentials so we are an anonymous user
+        self.client.credentials()
+        product_dict = {
+            "category": 1,
+            "title": "Asus TUF F-155",
+            "slug": 'asus_tuf_f-155',
+            "description": '8GB RAM 15.6" FHD display',
+            "price": 12000.00,
+            "in_stock": True,
+            "is_active": True,
+            "created_at": timezone.now()
+        }
+        resp = self.client.post("/api/v1/products/", product_dict)
+        self.assertEqual(resp.status_code, 401)
+        self.assertEqual(Product.objects.all().count(), 2)
+
+    def test_customer_product_create(self):
+        """
+        Check if a normal user can create a product
+        """
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.user_token.key)
+        product_dict = {
+            "category": 1,
+            "title": "Asus TUF F-155",
+            "slug": 'asus_tuf_f-155',
+            "description": '8GB RAM 15.6" FHD display',
+            "price": 12000.00,
+            "in_stock": True,
+            "is_active": True,
+            "created_at": timezone.now()
+        }
+        resp = self.client.post("/api/v1/products/", product_dict)
+        self.assertEqual(resp.status_code, 403)
+        self.assertEqual(Product.objects.all().count(), 2)
+
+    def test_seller_product_create(self):
+        """
+        Check if a seller user can create a product
+        """
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.seller_token.key)
+        product_dict = {
+            "category": 1,
+            "title": "Asus TUF F-515",
+            "slug": 'asus_tuf_f-155',
+            "description": '8GB RAM 15.6" FHD display',
+            "price": 12000.00,
+            "in_stock": True,
+            "is_active": True,
+        }
+        resp = self.client.post("/api/v1/products/", product_dict)
+        self.assertEqual(resp.status_code, 201)
+        self.assertEqual(Product.objects.all().count(), 3)
+
+    def test_post_create(self):
+        """
+        Check if the post created as excepted with POST request.
+        Superuser credentials used.
+        """
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.admin_token.key)
+        product_dict = {
+            "category": 1,
+            "title": "Asus TUF F-515",
+            "slug": 'asus_tuf_f33-155',
+            "description": '8GB RAM 15.6" FHD display',
+            "price": 12000.00,
+            "in_stock": True,
+            "is_active": True,
+            "created_at": timezone.now()
+        }
+        resp = self.client.post("/api/v1/products/", product_dict)
+        product_slug = resp.json()["slug"]
+        product_obj = Product.objects.get(slug=product_slug)
+
+        self.assertEqual(product_obj.title, product_dict["title"])
+        self.assertEqual(product_obj.slug, product_dict["slug"])
+        self.assertEqual(product_obj.description, product_dict["description"])
+        self.assertEqual(product_obj.price, float(product_dict["price"]))
+        self.assertEqual(product_obj.in_stock, product_dict["in_stock"])
+        self.assertEqual(product_obj.is_active, product_dict["is_active"])
+        self.assertEqual(product_obj.created_by, self.admin)
+
