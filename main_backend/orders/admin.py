04cf0a754de16b1368828faf2936c159950b7f80
abdoohossamm <abdoohossamm@outlook.com>
2022-12-01 22:39:35 +0200

    Add orders to admin panel and modify it

--- main_backend/orders/admin.py
+++ main_backend/orders/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from orders.models import Order
 
-# Register your models here.
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display_links = (
+        "created_by",
+        "payment_token",
+    )
+    list_display = (
+        "payment_token",
+        "created_by",
+        "phone",
+        "created_at",
+        "paid",
+        "cash_on_delivery",
+        "delivered",
+    )
+
+
+admin.site.register(Order, OrderAdmin)
