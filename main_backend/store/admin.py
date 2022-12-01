27b79149407a9b746e6486d2a37a9d1a9a0f7298
abdoohossamm <abdoohossamm@outlook.com>
2022-12-01 22:40:03 +0200

    Modify Album and Product for admin panel to show more fields

--- main_backend/store/admin.py
+++ main_backend/store/admin.py
@@ -2,6 +2,33 @@ from django.contrib import admin
 from store.models import Product, Album, Review, Category
 
 admin.site.register(Category)
-admin.site.register(Album)
+
+
+class AlbumAdmin(admin.ModelAdmin):
+    list_display_links = ("id",)
+    list_display = (
+        "id",
+        "image",
+        "created_by",
+        "content_type",
+        "object_id",
+        "content_object",
+    )
+
+
+admin.site.register(Album, AlbumAdmin)
 admin.site.register(Review)
-admin.site.register(Product)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display_links = ("id", "title",)
+    list_display = (
+        "id",
+        "title",
+        "created_by",
+        "thumbnail"
+    )
+
+
+admin.site.register(Product, ProductAdmin)
